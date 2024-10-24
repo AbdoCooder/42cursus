c7828b3b546515443819839c12b8735f9e704528
AbdoCooder <abdocooder@gmail.com>
2024-10-24 22:30:11 +0200

    24/10/2024 - 21:30

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,6 +3,22 @@
 
 void *ft_calloc(size_t nmemb, size_t size)
 {
-    if (nmemb == 0 || size == 0)
-        return (NULL)
+	size_t	totaleSize;
+	void	*ptr;
+
+	totaleSize = nmemb * size;
+	if (totaleSize == 0)
+	{
+		ptr = malloc(1);
+		if (!ptr)
+			return (NULL);
+		return (ptr);
+	}
+	if (totaleSize / nmemb != size)
+		return (NULL);
+	ptr = malloc(totaleSize);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, totaleSize);
+	return (ptr);
 }
