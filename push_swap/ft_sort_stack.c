e8c5b3dd1d556c402ff8f2f4d83251b4191279e2
Abenajib <abenajib@student.1337.ma>
2025-01-04 16:05:10 +0100

    automated push (:)

--- push_swap/ft_sort_stack.c
+++ push_swap/ft_sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: abenajib <abenajib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 11:00:30 by abenajib          #+#    #+#             */
-/*   Updated: 2024/12/31 20:14:34 by abenajib         ###   ########.fr       */
+/*   Updated: 2025/01/02 17:44:52 by abenajib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,22 @@ void	ft_find_target_in_a(t_list **ptr, t_list **stack_a)
 	(*ptr)->target = closest_bigger;
 }
 
+t_list	*ft_find_cheapest(t_list **stack)
+{
+	t_list	*cheapest;
+	t_list	*ptr;
+
+	cheapest = *stack;
+	ptr = *stack;
+	while (ptr)
+	{
+		if ((ptr->cost + ptr->target->cost) < (cheapest->cost + cheapest->target->cost))
+			cheapest = ptr;
+		ptr = ptr->next;
+	}
+	return (cheapest);
+}
+
 void	ft_set_indexs(t_list **stack_a, t_list **stack_b)
 {
 	int	i;
@@ -114,27 +130,13 @@ void	ft_set_costs(t_list **stack_a, t_list **stack_b)
 	}
 }
 
-t_list	*ft_find_cheapest(t_list **stack)
-{
-	t_list	*cheapest;
-	t_list	*ptr;
-
-	cheapest = *stack;
-	ptr = *stack;
-	while (ptr)
-	{
-		if ((ptr->cost + ptr->target->cost) < (cheapest->cost + cheapest->target->cost))
-			cheapest = ptr;
-		ptr = ptr->next;
-	}
-	return (cheapest);
-}
-
 void	ft_set_upper(t_list **stack)
 {
 	t_list	*ptr;
 	int		middle;
 
+	if (!(*stack))
+		return ;
 	middle = ft_lstsize(*stack) / 2;
 	ptr = *stack;
 	while (ptr)//loop on stack and set upper true for each node above the median
@@ -152,6 +154,7 @@ void	ft_move_cheapest_to_b(t_list **stack_a, t_list **stack_b)
 	t_list	*cheapest;
 
 	cheapest = ft_find_cheapest(stack_a);
+	// ft_printf("cheapest is [%d]\n", *(int *)cheapest->content);
 	ft_set_upper(stack_a);//set upper true for each node above the median
 	ft_set_upper(stack_b);
 	while (*stack_a != cheapest)
@@ -163,12 +166,48 @@ void	ft_move_cheapest_to_b(t_list **stack_a, t_list **stack_b)
 	}
 	while (*stack_b != cheapest->target)
 	{
-		if ((*stack_b)->target->upper == true)
+		if ((*stack_b)->upper == true)// it was [(*stack)->target->upper] and caused a segfault
 			rb(stack_b);
 		else
 			rrb(stack_b);
 	}
+	// ft_set_indexs(stack_a, stack_b);
+	// ft_printf("cheapest_index: %d \n", (cheapest)->index);
+	// ft_printf("cheapest_target_index: %d \n", (cheapest->target)->index);
 	pb(stack_a, stack_b);
+}
+
+void	ft_move_cheapest_to_a(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*cheapest;
+
+	cheapest = ft_find_cheapest(stack_b);
+	ft_set_upper(stack_a);
+	ft_set_upper(stack_b);
+	while (*stack_a != cheapest->target)
+	{
+		if (cheapest->target->upper == true)
+			ra(stack_a);
+		else
+			rra(stack_a);
+	}
+	pa(stack_a, stack_b);
+}
+
+void ft_print_info(t_list *stack_a, t_list *stack_b)
+{
+	ft_printf("stack_a\n");
+	while (stack_a)
+	{
+		ft_printf("index: %d | [%d] --> [%d] | cost : %d | upper? %s\n", (stack_a)->index, *(int *)(stack_a)->content, *(int *)((stack_a)->target)->content, (stack_a)->cost, (stack_a)->upper ? "yes" : "no");
+		stack_a = stack_a->next;
+	}
+	ft_printf("stack_b\n");
+	while (stack_b)
+	{
+		ft_printf("index: %d | [%d] --> [%d] | cost : %d | upper? %s\n", (stack_b)->index, *(int *)(stack_b)->content, *(int *)((stack_b)->target)->content, (stack_b)->cost, (stack_b)->upper ? "yes" : "no");
+		stack_b = stack_b->next;
+	}
 }
 
 void	ft_sort_stack(t_list **stack_a, t_list **stack_b)
@@ -186,24 +225,28 @@ void	ft_sort_stack(t_list **stack_a, t_list **stack_b)
 		while (ft_lstsize(*stack_a) > 3 && counter++ < 2)
 			pb(stack_a, stack_b);
 		ptr = *stack_a;
-		while (ptr)
+		while (ptr)// after this loop every node on a has a target in b
 		{
 			ft_find_target_in_b(&ptr, stack_b);
 			ptr = ptr->next;
-		}// after this loop every node on a has a target in b
-		// while (ft_lstsize(*stack_a) > 3) // move the cheapest node to b until still three in a
-		// {
+		}
+		while (ft_lstsize(*stack_a) > 3) // move the cheapest node to b until still three in a
+		{
 			ft_set_costs(stack_a, stack_b);
 			ft_move_cheapest_to_b(stack_a, stack_b);
-			ft_set_costs(stack_a, stack_b);
-			ft_printf("index: %d | [%d] --> [%d] | cost : %d | upper? %s\n", (*stack_a)->index, *(int *)(*stack_a)->content, *(int *)((*stack_a)->target)->content, (*stack_a)->cost, (*stack_a)->upper ? "yes" : "no");
-		// }
-		// ft_sort_three(stack_a);
+		}
+		ft_sort_three(stack_a);
 		ptr = *stack_b;
-		while (ptr)
+		while (ptr)// after this loop every node on b has a target in a
 		{
 			ft_find_target_in_a(&ptr, stack_a);
 			ptr = ptr->next;
-		}// after this loop every node on b has a target in a
+		}
+		while (*stack_b) // move the cheapest node to a until still nothing in b
+		{
+			ft_set_costs(stack_a, stack_b);
+			ft_move_cheapest_to_a(stack_a, stack_b);
+		}
 	}
 }
+//ft_printf("index: %d | [%d] --> [%d] | cost : %d | upper? %s\n", (*stack_a)->index, *(int *)(*stack_a)->content, *(int *)((*stack_a)->target)->content, (*stack_a)->cost, (*stack_a)->upper ? "yes" : "no");
